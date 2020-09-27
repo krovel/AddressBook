9a72e2d0b7ae3cf57f245fd9d6205c4d1c85bf93
krove <krove@DESKTOP-Q5SED1R>
2020-09-27 22:19:33 +0530

    [Kashif]Contact created

--- src/com/capgemini/AddressBook.java
+++ src/com/capgemini/AddressBook.java
@@ -1,7 +1,28 @@
 package com.capgemini;
 
 public class AddressBook {
-public static void main(String[] args) {
-	System.out.println("Welcome to the AddressBook");
-}
+	public static String Fname,Lname,Email,Add,City,State,Pno;
+	public static long Zip;
+	public AddressBook(String fname,String lname,String email,String add,String city,String state,long zip,String pno) {
+		Fname = fname;
+		Lname = lname;
+		Email = email;
+		Add = add;
+		City = city;
+		State = state;
+		Zip = zip;
+		Pno = pno;
+	}
+	public void display() {
+		System.out.println("Name- "+Fname+" "+Lname);
+		System.out.println("Email- "+Email);
+		System.out.println("Address- "+Add+" "+City+" "+State);
+		System.out.println("Zip- "+Zip);
+		System.out.println("Phone no- "+Pno);
+		}
+	public static void main(String[] args) {
+		System.out.println("Welcome to AddressBook");
+		AddressBook c1= new AddressBook("Mohd Kashif","Ansari","matrixkashif@gmail.com","Meer Anees Lane, Chowk,","Lucknow,","Uttar Pradesh",226003,"9598252500");
+		c1.display();
+	}
 }
